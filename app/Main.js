2086ccf0a4e8b3e2e37b34e0326e4d5bbe2689dc
unknown <Administrator@DMARB0201C697WX.student.arteveldehs.be>
2019-02-25 11:37:42 +0100

    Add colors styles

--- app/Main.js
+++ app/Main.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
 import { LinearGradient } from 'expo'
 
+import { primaryGradientArray } from './utils/styles/Colors'
 import { Header } from './components'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+  centered: {
+      alignItems: 'center'
+  }
 })
 
 export class Main extends Component {
@@ -31,11 +35,14 @@ export class Main extends Component {
     render() {
         const { title, color } = this.props
         const { time } = this.state
-        const grArray = ['#ff0000', '#0000ff']
 
         return (
-            <LinearGradient colors={ grArray } style={styles.container}>
-                <View style={styles.container}>
+            <LinearGradient colors={ primaryGradientArray } style={styles.container}>
+                <StatusBar barStyle="light-content" />
+                <View style={ styles.centered }>
+                    <Header title='NMD To Do' />
+                </View>
+                <View style={ styles.container }>
                     <Text>{ title } in { color }</Text>
                     <Text>{ time }</Text>
                     <Text>NMD - Open up App.js to start working on your app!</Text>
---
+++ app/utils/styles/Colors.js
@@ -0,0 +1,3 @@
+export const red = '#FF5630'
+export const blue = '#00B8D9'
+export const primaryGradientArray = [red, blue]
